b3f07ce6dda5d2258ae978bc4f3d1a7fd9c6b4fb
odpuentesg <odpuentesg@gmail.com>
2025-03-31 20:44:15 -0500

    Fix: Configuración para solucionar error 404 en ruta /admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,13 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
 	site: 'https://abyayala.org',
 	integrations: [mdx(), sitemap(), tailwind()],
+	outDir: './dist',
+	publicDir: './public',
+	build: {
+		// Asegurarse de que los archivos en public/ se copien a la carpeta de salida
+		assets: '_assets',
+	},
+	// Configuración para manejar rutas SPA como /admin
+	output: 'static',
+	trailingSlash: 'ignore',
 });
